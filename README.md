05296cdc84f195dd7f3448f6879885e07c116d4a
AwwalaK <awwalakamila01@gmail.com>
2023-08-05 11:55:41 +0700

    feat: update pagination

--- README.md
+++ README.md
@@ -7,3 +7,17 @@
 1. Go to backend foler
 2. if you do not have database, run php artisan migrate
 3. run php artisan serve
+
+4. Go to frontend folder
+5. npm intall
+6. npm run
+
+## Make sure ENV in backend folder similar like this
+you can change username and password with yours
+
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3308
+DB_DATABASE=sharingvision
+DB_USERNAME=root
+DB_PASSWORD=
--- frontend/src/routes/PreviewPost.js
+++ frontend/src/routes/PreviewPost.js
@@ -2,49 +2,66 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { IoMdArrowBack } from "react-icons/io"; 
 
-import Pagination from '../components/Pagination';
-
 import './PreviewPost.css';
 
 const Preview = () => {
   const [publishedPosts, setPublishedPosts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const postsPerPage = 5;
+  const [limit, setLimit] = useState(10);
+  const [offset, setOffset] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch('http://127.0.0.1:8000/api/articles')
-      .then((res) => res.json())
-      .then((res) => setPublishedPosts(res))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, [publishedPosts]);
+    const fetchArticles = async () => {
+      try {
+        const responseAwal = await fetch(`http://127.0.0.1:8000/api/articles`);
+        const response = await fetch(`http://127.0.0.1:8000/api/articles/${limit}/${offset}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch articles.');
+        }
 
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = publishedPosts.slice(indexOfFirstPost, indexOfLastPost);
+        const data = await response.json();
+        const dataAwal = await responseAwal.json();
+        setPublishedPosts(data);
+        setTotalPages(dataAwal.length);
+      } catch (error) {
+        console.error('Error fetching articles:', error);
+      }
+    };
 
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+    fetchArticles();
+  }, [limit, offset]);
 
+  const handleNextPage = () => {
+    if (limit + 1 < totalPages) {
+      setOffset(offset+limit);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (limit - 1 >= 0) {
+      setOffset(offset-limit);
+    }
+  };
+  
   return (
     <div className='preview-post'>
       <div className='back-button' onClick={()=>navigate('/')}>
         <IoMdArrowBack size={30}/>Back
       </div>
       <h1>Preview</h1>
-      {currentPosts.map((post) => (
+      {(publishedPosts?.filter((x) => x?.status === 'publish'))?.map((post) => (
         <div key={post.id} className='post-card'>
           <h3>{post.title}</h3>
           <p>{post.content}</p>
           <p>Category: {post.category}</p>
         </div>
       ))}
-
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={publishedPosts.length}
-        paginate={paginate}
-      />
+      
+      <button onClick={handlePreviousPage}>Previous Page</button>
+      <button onClick={handleNextPage}>Next Page</button>
+    
     </div>
   );
 };
